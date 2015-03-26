190a895ed2187aa05b08e82a0233471b4d5f3be0
denniseik <denniseik@bitbucket.org>
2015-03-26 13:46:35 +0100

    Ready to go

---
+++ basic/views/site/index.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Horus.TV</h1>
+
+        <p class="lead">in collaboration wih Trentino TV</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Show demo</a></p>
+    </div>
+
+</div>
